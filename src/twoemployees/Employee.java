e2de46fa4d572a4b61389ee0766f5d687e26d560
ljsnell <ljsnell7@gmail.com>
2022-09-05 13:54:55 -0500

    Added place holder code for two employees in class work.

---
+++ src/twoemployees/Employee.java
@@ -0,0 +1,34 @@
+package twoemployees;
+
+public class Employee {
+	private int empNum;
+	private String empLastName;
+	private String empFirstName;
+	private double empSalary;
+	
+	public int getEmpNum() {
+		return empNum;
+	}
+	public void setEmpNum(int empNum) {
+		this.empNum = empNum;
+	}
+	public String getEmpLastName() {
+		return empLastName;
+	}
+	public void setEmpLastName(String empLastName) {
+		this.empLastName = empLastName;
+	}
+	public String getEmpFirstName() {
+		return empFirstName;
+	}
+	public void setEmpFirstName(String empFirstName) {
+		this.empFirstName = empFirstName;
+	}
+	public double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+	
+}
